f56a33c3298252b79ddc6267be09d6b1891fb596
Itohaneliza <juliuseli2004@gmail.com>
2023-11-24 12:23:39 +0100

    first commit

---
+++ controller/emailCtrl.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+const asyncHandler = require("express-async-handler");
+
+const sendEmail = asyncHandler(async(data, req, res)=> {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, 
+        auth: {
+            user : process.env.MAIL_ID,
+            pass : process.env.MP,
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Hey 👻" <sisi@gmail.com>',
+         to: data.to,
+        subject: data.subject,
+        text: data.text,
+        html: data.htm,
+  });
+
+  console.log("Message sent: %s", info.messageId);
+
+console.log("preview URL: %s", nodemailer.getTestMessageUrl(info));
+})
+module.exports = sendEmail;
